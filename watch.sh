0f3f91255bcb946565815ef42b3c0241171b11b5
Chuck Ha <ha.chuck@gmail.com>
2013-03-16 23:06:36 -0400

    remove names

--- watch.sh
+++ watch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 username=`whoami`
-site="http://www.singerco.com/accessories/detail/2093/darning-embroidery-foot"
-look_for="/images/btn_addtocart.gif"
-subject="Order the darn foot"
-body="Order the foot from $site"
+site=""
+look_for=""
+subject=""
+body=""
 
 # -s makes curl silent. Pipe HTML to grep, count the finds
 
@@ -12,5 +12,5 @@ count=`curl -s $site | grep $look_for | wc -l`
 
 if [ $count -gt 0 ]
 then
-        `echo "$body" | mail -u $username "$subject"`
+        `echo "$body" | mail -s "$subject" $username`
 fi
